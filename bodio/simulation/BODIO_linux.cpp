c49ae2c2633ea9fd4030c7046b460c118939de23
nsygtc <68743327+nsygtc@users.noreply.github.com>
2026-06-02 13:47:08 +0800

    Update and rename bGWO-CPA_linux.cpp to BODIO_linux.cpp

--- bodio/simulation/BODIO_linux.cpp
+++ bodio/simulation/BODIO_linux.cpp
@@ -80,18 +80,6 @@ void bGWO(unsigned char* key, int trace_num, string start, int pn, int evolution
 			memcpy(alpha.key, w[1].key, 16);
 			memcpy(beta.key, w[2].key, 16);
 			memcpy(delta.key, w[3].key, 16);
-/*
-			printf("%lf  %lf  %lf  %f  %f ", w[1].corr, w[2].corr, w[3].corr, w[4].corr, w[5].corr);
-			for (int j = 0; j < 16; j++) {
-				printf("%02X", alpha.key[j]);  //%02X输出为16进制数
-			}
-			printf(" %d\n", dis(alpha.key, Data[1].key));//比较正确密钥和当前密钥x.key差几比特*/
-
-			//fprintf(fp, "%d,%d\n", i, (16 - dis_byte(best.key, Data[1].key)));
-			//fprintf(fp, "%d,%d\n", i, (128 - dis(alpha.key, Data[1].key)));
-		}
-		memcpy(key, alpha.key, 16);
-		/*
 		printf("第%d次实验的代数为：%d\n", k, i - 1);
 		if (i == evolution_max + 1) {
 			//fprintf(fp, "fail\n");
@@ -163,14 +151,7 @@ void bGWO(unsigned char* key, int trace_num, string start, int pn, int evolution
 
 	double stddev_time = sqrt(variance_time);
 
-	//fprintf(fp, "Success Number,%d,k_2,%d,Average Evolution Generation,%f,%f,Process Time,%f\n", (k - 1) - fail_num, k_2, (double)(count + count_fail) / (double)(k - 1), (double)(count_2 + 10 * fail_num) / (double)(k_2), timeuse);
 
-
-	//printf("实验成功次数为：%d, 平均进化代数为：%f，computation_fitness为：%d，computation_sort为：%d\n", (k - 1) - fail_num, (double)(count + evolution_max * fail_num) / (double)(k - 1), computation_fitness, computation_sort);  //输出平均代数
-	//fprintf(fp, "Success Number,%d,Average Evolution Generation,%f,computation_fitness,%d,computation_sort,%d\n", (k - 1) - fail_num, (double)(count + evolution_max * fail_num) / (double)(k - 1), computation_fitness, computation_sort);
-
-
-	//fclose(fp);
 	double success_num = (double)experiment_max - (double)fail_num;
 	double success_rate = success_num / experiment_max;
 	//枚举
@@ -191,7 +172,7 @@ void bGWO(unsigned char* key, int trace_num, string start, int pn, int evolution
 	
 }
 
-//thread th[1];  //20
+
 int pn_set[5] = {8, 7, 6, 5, 4};
 
 int main() {
@@ -203,25 +184,6 @@ int main() {
 	//gettimeofday(&tpstart,NULL);
 
 	int trace_number_min = 50, trace_number_max = 1000, trace_number_step = 50, pn, evolution_max = 3000, flag_num = 100, experiment_max = 200;   // 50 1000 50 
-/*
-	for (int k = 0; k < 1; k++) {
-		//pn = pn_set[k];
-		pn = 5;
-		for (int i = trace_number_min; i <= trace_number_max; i += trace_number_step) {  
-
-			const int n = i;////n表示生成的波形数量，读取n次加密结果
-			unsigned char best_key[16];
-			printf("Trace number is %d\n", n);
-
-			//th[0] = thread(bGWO, best_key, n, start, pn, evolution_max);
-			th[(i / trace_number_step) - 1] = thread(bGWO, best_key, n, start, pn, evolution_max);
-		}
-
-		//for (int i = 0; i < 1; i++) {
-		for(int i = 0; i < trace_number_max / trace_number_step; i++){
-			th[i].join();
-		}
-	}*/
 
 
 	for (int k = 0; k < 5; k++) {
@@ -261,19 +223,8 @@ int main() {
 		}
 
 		fclose(fp1);
-/*
-		for (int i = 0; i < 1; i++) {
-			th[0].join();
-		}*/
+
 	}
-
-
-	//gettimeofday(&tpend,NULL);
-	//timeuse=1000000*(tpend.tv_sec-tpstart.tv_sec)+tpend.tv_usec-tpstart.tv_usec;
-	//timeuse/=1000000;
-	//printf("Used Time:%f\n",timeuse);
-
-	/*printf("%s\n%s\n", start.c_str(), currentTimetoStr().c_str());*/
 	printf("--------------Experiments End---------------\n");
 
 	return 0;
