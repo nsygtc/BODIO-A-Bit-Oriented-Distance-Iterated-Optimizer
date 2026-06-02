4f344d5dcccfca4a908ce86ea759598f623db405
nsygtc <68743327+nsygtc@users.noreply.github.com>
2026-06-02 14:45:47 +0800

    Update BODIO-Real_linux.cpp

--- bodio/real/BODIO-Real_linux.cpp
+++ bodio/real/BODIO-Real_linux.cpp
@@ -34,8 +34,7 @@ void bGWO(unsigned char* key, int trace_num, string start, int pn, int evolution
 	double trace[1][maxn];//trace[x][y]在第y个波形里  第x个点的值
 	int HD_key[maxn];
 
-	for (k = 1; k <= experiment_max; k++) {  
-		//srand(time(0));  
+	for (k = 1; k <= experiment_max; k++) {   
 
 		int start_trace = start_trace_set[k];
 
@@ -45,24 +44,20 @@ void bGWO(unsigned char* key, int trace_num, string start, int pn, int evolution
 		gettimeofday(&tpstart, NULL);
 
 		creat_node(w, pn, trace_num, Data, HD_key, trace);
-		/*for (int m = 1; m < pn; m++) {
-			printf("%lf   ", w[m].corr);
-		}*/
+
 		node alpha, beta, delta;
-		//best.corr = 0;
+
 		alpha.corr = 0;
 		beta.corr = 0;
 		delta.corr = 0;
 
-		//node correct;
-		//memcpy(correct.key, Data[1].key, 16);
-		//correct.corr = calc_corr_SboxPre(correct.key, trace_num, Data, HD_key, trace);
+
 
 		memcpy(alpha.key, w[1].key, 16);
 		memcpy(beta.key, w[2].key, 16);
 		memcpy(delta.key, w[3].key, 16);
 
-		//枚举
+		
 		for (i = 1; memcmp(alpha.key, Data[1].key, 16) && memcmp(beta.key, Data[1].key, 16) && memcmp(delta.key, Data[1].key, 16) && flag != flag_num; i++) {
 
 			work_bGWO(trace_num, w, Data, HD_key, trace, pn, evolution_max, i);
@@ -78,17 +73,9 @@ void bGWO(unsigned char* key, int trace_num, string start, int pn, int evolution
 				flag = 0;
 			}
 
-			//best = min(best, w[1]);
 			memcpy(alpha.key, w[1].key, 16);
 			memcpy(beta.key, w[2].key, 16);
 			memcpy(delta.key, w[3].key, 16);
-
-			//printf("%lf  %lf  %lf  %f  %f ", correct.corr, w[1].corr, w[2].corr, w[3].corr, w[4].corr);
-			//for (int j = 0; j < 16; j++) {
-			//	printf("%02X", alpha.key[j]);  
-			//}
-			//printf(" %d\n", dis(alpha.key, Data[1].key));
-
 		}
 		memcpy(key, alpha.key, 16);
 
@@ -103,29 +90,19 @@ void bGWO(unsigned char* key, int trace_num, string start, int pn, int evolution
 				//printf("%d ", j);
 				precise_search(alpha, w_precise, pn, trace_num, Data, HD_key, trace);
 
-				//printf("%lf  %lf  %lf  %lf  ", w_precise[1].corr, w_precise[2].corr, w_precise[3].corr, w_precise[4].corr);
-				//best = min(best, w_precise[1]);
-				//memcpy(best.key, w_precise[1].key, 16);
-				//best.corr = calc_corr_SboxPre(best.key, trace_num, Data, HW_key, trace);
-
 				memcpy(alpha.key, w_precise[1].key, 16);
 				memcpy(beta.key, w_precise[2].key, 16);
 				memcpy(delta.key, w_precise[3].key, 16);
-				//for (int j = 0; j < 16; j++) {
-				//	printf("%02X", alpha.key[j]);  //%02X输出为16进制数
-				//}
-				//printf(" %d\n", dis(alpha.key, Data[1].key));//比较正确密钥和当前密钥x.key差几比特
+
 			}
 
 			if (j == 10 + 1) {
-				//fprintf(fp, "fail\n");
 				fail_num++;
 				count_fail += i - 1;
 			}
 			else {
 				count += i - 1;
 				count_2 += j - 1;
-				//fprintf(fp, "success\n");
 			}
 		}
 
@@ -140,7 +117,6 @@ void bGWO(unsigned char* key, int trace_num, string start, int pn, int evolution
 
 	}
 	
-	//计算runtime均值和标准差
 
 	double mean_time = total_runtime / experiment_max;
 	double variance_time = 0.0;
@@ -163,18 +139,14 @@ void bGWO(unsigned char* key, int trace_num, string start, int pn, int evolution
 	}
 
 	fprintf(fp1, "%d,%f,%f,%f,%.6f,%.6f,%.6f", trace_num, success_rate, (double)(count + count_fail) / (double)(k - 1), ave_onebitflip, total_runtime, mean_time, stddev_time);
-	//fprintf(fp1, "%d,%f,%f,%d,%d\n", trace_num, result / (double)experiment_max, (double)(count + evolution_max * fail_num) / (double)(k - 1), computation_fitness, computation_sort);
 	for (int i = 0; i < experiment_max; i++)
 	{
 		fprintf(fp1, ",%.6f", runtimes[i]);
 	}
 	fprintf(fp1, "\n");
-
-	printf("实验成功次数为：%d, k_2次数：%d，平均进化代数为：%f + %f，实验运行时间：%f s\n", (int)success_num, k_2, (double)(count + count_fail) / (double)(k - 1), ave_onebitflip, total_runtime);  //输出平均代数
-
+	printf("实验成功次数为：%d, k_2次数：%d，平均进化代数为：%f + %f，实验运行时间：%f s\n", (int)success_num, k_2, (double)(count + count_fail) / (double)(k - 1), ave_onebitflip, total_runtime);  
 
 }
-
 
 vector<int> pn_set = { 4, 5, 6, 7, 8 };
 
@@ -187,7 +159,6 @@ int main() {
 
 	for (int k = 0; k < pn_set.size(); k++) {
 		pn = pn_set[k];
-		//pn = 8;
 
 		string dir = "bGWO-CPA-Real/" + start + "/" + "pn" + to_string(pn) + "/";  
 		CreateFolder(dir);
